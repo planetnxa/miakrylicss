fd78c9890aa4a44e03040b5b56dfed04f753c6ba
covergirlszn <marielarte26@gmail.com>
2023-05-09 00:36:41 +0100

    -

---
+++ main.js
@@ -0,0 +1,31 @@
+bService
+let bService = {
+    name: "handacrylics",
+    price: 45,
+    addons: ["some list"],
+    eyeColor: "blue"
+  };
+  /*"""on div click set value to hand
+now when they click on the crylics div, create a new object
+acrylic = doc.get element by id. acrylic hand id thing
+onclick....
+  create the acrylic object with bservice
+  price = 45
+  addons list
+
+  now you will open the list 
+
+
+
+*/
+
+
+//you might need to simplify it yk ,just so you can at least get it done. because all of it 
+//is the same but you give yourself extra work for the sake of aesthetics. it's okay
+//might just do hand
+//then make the feet section non selectable, coming soon!
+const addonsList = ["xl length", "french tip", "gems", "ft removal","custom design"];
+if(selected service == hands)
+let addons = addonsList[full thing]
+
+serviceType = document.getElement()

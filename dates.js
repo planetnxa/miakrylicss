42041447d78aec491c4063e880a116796dbd289a
covergirlszn <marielarte26@gmail.com>
2023-05-13 18:11:40 +0100

    more js updates

--- dates.js
+++ dates.js
@@ -1,4 +1,21 @@
+const months = ["January", "February", "March", "April", "May", 
+"June", "July", "August", "September", "October", "November", "December"];
+
+
+
 $("#conf").click(function(){
-    var yes = $("#datee").val(); //val not value
-    $("#apptdate").text(yes);
+    var d = new Date($("#datee").val());
+datee = d.getDate() + " "+ months[d.getMonth()]+ " "+ d.getFullYear();
+     //val not value
+
+    var t = $("#timee").val();
+        $("#apptdate").text(datee);
+        $("#apptime").text(t);
+        final = datee +", "+t;
+        $("#conf").text("confirm") //if done correct...check for undeffined
+
     });
+
+    sessionStorage.setItem("date")
+    //gonna hand it in late idk
+//    2 days is FileSystemEntry. i'm disappointed in myself but i need to rest
